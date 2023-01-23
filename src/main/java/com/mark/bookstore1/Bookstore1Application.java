e75a051bcf8b9e48c519ff19e4cb15f6cef7c635
iufuture7 <iufuture@naver.com>
2023-01-23 18:26:19 +0900

    초기 파일 버전

---
+++ src/main/java/com/mark/bookstore1/Bookstore1Application.java
@@ -0,0 +1,13 @@
+package com.mark.bookstore1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookstore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bookstore1Application.class, args);
+	}
+
+}
